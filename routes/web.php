ebbb12e8a4772885eb29baf90bc8e35523dab547
DrewNovansky <novanskyi@gmail.com>
2021-01-08 14:06:10 +0700

    Views

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 Route::get('/appointment', function () {
     return view('mc_appointment');
 });
+
+Route::get('/register', 'RegistrationController@create');
+
+Route::post('register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+
+Route::post('/login', 'SessionsController@store');
+
+Route::get('/logout', 'SessionsController@destroy');
